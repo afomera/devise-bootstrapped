97488195cd2953b3bd7b4f9f33f03517bb6ae44f
Larry Reid <lcreid@jadesystems.ca>
2018-08-16 14:21:24 -0700

    Give up on testing for now.

--- test/bootstrap_form_test.rb
+++ test/bootstrap_form_test.rb
@@ -2,8 +2,15 @@
 
 require "test_helper"
 
-class BootstrapFormTest < MiniTest::Test
+class BootstrapFormTest < Test
   def test_has_a_version_number
     refute_nil Devise::BootstrapForm::VERSION
   end
+
+  # The following will try to render the template, but will fail because it depends
+  # on a lot coming from the Devise controller.
+  # def test_sign_in_view
+  #   ActionController::Base.render(file: File.join(@view_root, "registrations", "new"))
+  #   flunk
+  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,20 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
+require "action_view"
+
+class Test < ActionView::TestCase
+  class User
+    attr_reader :email, :name, :password
+
+    def initialize
+      @email = "email@example.com"
+      @name = "name"
+      @password = "password"
+    end
+  end
+
+  setup do
+    @view_root = File.join("app", "views", "devise")
+  end
+end
