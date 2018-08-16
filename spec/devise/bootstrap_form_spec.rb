abcb5a5820057e194fa3bc084cd1871fe910af6d
Larry Reid <lcreid@jadesystems.ca>
2018-08-16 11:49:01 -0700

    Remove a null test.

--- spec/devise/bootstrap_form_spec.rb
+++ spec/devise/bootstrap_form_spec.rb
@@ -4,8 +4,8 @@ describe Devise::BootstrapForm do
   it 'has a version number' do
     expect(Devise::BootstrapForm::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  #
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
 end
