e2b5add280cf5c3d348d8001f1acdedb081f7e45
Larry Reid <lcreid@jadesystems.ca>
2018-08-23 10:33:39 -0700

    Try another way of starting Chrome.

--- test/rails_app/test/application_system_test_case.rb
+++ test/rails_app/test/application_system_test_case.rb
@@ -11,10 +11,15 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.server_port = 3001
   Capybara.app_host = "http://localhost:3001"
 
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  driven_by :selenium,
+    using: :headless_chrome,
+    options: { args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"] },
+    screen_size: [1400, 1400]
 
   def setup
     super
+    # puts "Tasks: #{Rake::Task.tasks}"
+    # Rake.application["devise:views:bootstrap_form"].tap(&:invoke).tap(&:reenable)
     `cd test/rails_app; rails g devise:views:bootstrap_form -f`
   end
 end
