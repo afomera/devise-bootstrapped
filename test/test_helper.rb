81cd6ff16b75b99fe229713480778e3b80c40818
Larry Reid <lcreid@jadesystems.ca>
2019-03-15 12:35:38 -0700

    Use local tmp for test output.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,14 +8,13 @@ require "devise_bootstrap_form"
 
 class Test < Rails::Generators::TestCase
   tests Devise::Views::BootstrapFormGenerator
-  destination File.expand_path("/tmp", __dir__)
+  destination File.expand_path("../tmp", __dir__)
 
-  setup :prepare_destination
-
-  # AssertGeneratorTest, this makes the test work.
+  # For AssertGeneratorTest, this makes the test work.
   # For others, it silences a warning message that might be confusing to developers.
   setup do
-    FileUtils.mkdir_p("/tmp/app/assets/stylesheets")
-    FileUtils.touch("/tmp/app/assets/stylesheets/application.scss")
+    prepare_destination
+    FileUtils.mkdir_p(File.join(destination_root, "app/assets/stylesheets"))
+    FileUtils.touch(File.join(destination_root, "app/assets/stylesheets/application.scss"))
   end
 end
